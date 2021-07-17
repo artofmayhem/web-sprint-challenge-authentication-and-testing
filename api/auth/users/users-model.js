c8f1264cd928ad558790654a4901dff706c81428
artofmayhem <chef_tony_miller@yahoo.com>
2021-07-17 09:01:58 -1000

    scaffolding set, users-model secrets, deps in

---
+++ api/auth/users/users-model.js
@@ -0,0 +1,32 @@
+const db = require('../../../data/dbConfig');
+
+function find() {
+    return db('users as u')
+        .select('*')
+}
+
+function findByUserName(userName) {
+    return db('users as u')
+        .select('*')
+        .where('u.userName', userName)
+}
+
+function findById(id) {
+    return db('users as u')
+        .select('*')
+        .where('u.id', id)
+}
+
+function create(newUser) {
+    const [id] = db('users as u')
+        .insert(newUser)
+        return findById(id)
+}
+
+
+module.exports = {
+    find: find,
+    findByUserName: findByUserName,
+    findById: findById,
+    create: create
+}
